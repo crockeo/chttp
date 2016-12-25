47cf2da37bce6a7442fa2f386a7f29daa911252d
Cerek Hillen <crockeo@gmail.com>
2016-12-25 17:01:26 -0500

    Starting to implement fmemopen on mac.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # chttp
 
 Implementation of HTTP header parsing.
+
+### Credits
+
+Inspiration for my macOS `funopen(...)` implementation comes from
+[NimbusKit/memorymapping](https://github.com/NimbusKit/memorymapping).
--- src/parse.c
+++ src/parse.c
@@ -2,18 +2,63 @@
 
 #if defined(__APPLE__) || defined(__WIN32)
 #include <stdlib.h>
-// TODO: Implement core support for fmemopen implementations.
+#include <string.h>
 #endif
 
 #ifdef __APPLE__
-static FILE *fmemopen(const char *string, size_t len, const char *mode)
+struct mem_region
 {
-    // TODO: Implement.
-    printf("fmemopen unimplemented on APPLE.\n");
-    exit(1);
+    size_t position;
+    size_t size;
+    void *buffer;
+};
+typedef struct mem_region mem_region_t;
+
+int read_fn(void *file, char *buf, int nbytes)
+{
+    mem_region_t *mem = (mem_region_t *)file;
+    size_t available = mem->size - mem->position;
+    if (nbytes > available)
+        nbytes = available;
+
+    memcpy(buf, mem->buffer + mem->position, nbytes);
+    size_t pos = mem->position;
+    mem->position += nbytes;
+
+    return mem->position - pos;
+}
+
+int write_fn(void *file, const char *buf, int nbytes)
+{
+    mem_region_t *mem = (mem_region_t *)file;
+    size_t available = mem->size - mem->position;
+    if (nbytes > available)
+        nbytes = available;
+
+    memcpy(mem->buffer + mem->position, buf, available);
+    size_t pos = mem->position;
+    mem->position += nbytes;
+
+    return mem->position - pos;
+}
+
+off_t seek_fn(void *file, off_t offset, int whence)
+{
+    return 0;
+}
+
+int close_fn(void *file)
+{
+    return 0;
+}
+
+static FILE *fmemopen(void* buffer, size_t len, const char *mode)
+{
+    mem_region_t *mem = (mem_region_t *)malloc(sizeof(mem_region_t));
+    return funopen(mem, &read_fn, &write_fn, &seek_fn, &close_fn);
 }
 #elif __WIN32
-static FILE *fmemopen(const char *string, size_t len, const char *mode)
+static FILE *fmemopen(void *buffer, size_t len, const char *mode)
 {
     // TODO: Implement.
     printf("fmemopen unimplemented on WIN32.\n");
@@ -41,7 +86,7 @@ int chttp_parse_response(chttp_response *r, FILE *f)
 // Pipes string to a FILE * and calls chttp_parse_request.
 int chttp_sparse_request(chttp_request *r, const char *string, int len)
 {
-    FILE *f = fmemopen(string, len, "r");
+    FILE *f = fmemopen((char *)string, len, "r");
     if (f == NULL)
         return -1;
 
@@ -53,7 +98,7 @@ int chttp_sparse_request(chttp_request *r, const char *string, int len)
 // Pipes string to a FILE * and falls chttp_parse_response.
 int chttp_sparse_response(chttp_response *r, const char *string, int len)
 {
-    FILE *f = fmemopen(string, len, "r");
+    FILE *f = fmemopen((char *)string, len, "r");
     if (f == NULL)
         return -1;
 
